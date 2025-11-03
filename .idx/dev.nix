9ec82b632ad107ea7b37c1441aba31aeb5e1fe26
mashoun.dev <mashoun.dev@gmail.com>
2025-11-03 16:28:43 +0000

    fixed some nix options

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -16,7 +16,7 @@
     ];
     # Enable previews
     previews = {
-      enable = true;
+      enable = false;
       previews = {
         web = {
           command = ["npm" "run" "dev" "--" "--port" "$PORT"];
