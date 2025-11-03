45b861671636a8cd8715f77f2dec3b026e033bdf
mashoun.dev <mashoun.dev@gmail.com>
2025-11-03 08:18:14 +0000

    testing something

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,4 @@
 <template>
     <h1>Index page</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime laudantium sunt eos aut. Nostrum beatae deserunt iure. Incidunt enim blanditiis ea repellat obcaecati mollitia, neque ab, voluptatem nemo consectetur nihil!</p>
 </template>
